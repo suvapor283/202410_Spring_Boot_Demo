c7c23a0229e3547f86700577423f984304269693
suvapor283 <suvapor8632@gmail.com>
2024-10-10 22:52:43 +0900

    sql article-mapper apply

--- src/main/java/com/example/basic/ArticleDao.java
+++ src/main/java/com/example/basic/ArticleDao.java
@@ -1,29 +1,12 @@
 package com.example.basic;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ArticleDao {
 
-    @Insert("""
-            INSERT INTO article
-                SET title = #{title}
-                    , `body` = #{body}
-            """)
-    void save(String title, String body);
-
-    @Select("""
-            SELECT * FROM article
-            """)
-    List<Article> findAll();
-
-    @Select("""
-            SELECT * FROM article
-                WHERE id = #{id}
-            """)
+    void save(@Param("title") String title, @Param("body") String body);
+//    List<Article> findAll();
     Article findById(Long id);
 }
