9b2e70def0a03ce2cd5d1018f64aa09369adbc42
suvapor283 <suvapor8632@gmail.com>
2024-10-10 23:07:08 +0900

    article write, list, detail refactoring

--- src/main/java/com/example/basic/ArticleDao.java
+++ src/main/java/com/example/basic/ArticleDao.java
@@ -3,10 +3,12 @@ package com.example.basic;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface ArticleDao {
 
     void save(@Param("title") String title, @Param("body") String body);
-//    List<Article> findAll();
+    List<Article> findAll();
     Article findById(Long id);
 }
--- src/main/java/com/example/basic/TestController.java
+++ src/main/java/com/example/basic/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Controller
@@ -14,81 +14,27 @@ public class TestController {
 
     private final ArticleDao articleDao;
 
-    @RequestMapping("/t8/{id}")
+    @RequestMapping("/article/detail/{id}")
     @ResponseBody
-    public Article t8(@PathVariable("id") long id){
+    public Article detail(@PathVariable("id") long id){
         Article article = articleDao.findById(id);
 
         return article;
     }
 
-    @RequestMapping("/t7")
+    @RequestMapping("/article/list")
     @ResponseBody
-    public Article t7(long id){
-        Article article = articleDao.findById(id);
-
-        return article;
-    }
-
-    @RequestMapping("/t6")
-    @ResponseBody
-    public List<Article> t6(){
+    public List<Article> list(){
         List<Article> articleList = articleDao.findAll();
 
         return articleList;
     }
 
-    @RequestMapping("/t5")
+    @RequestMapping("/article/write")
     @ResponseBody
-    public String t5(String title, String body){
+    public String write(String title, String body){
         articleDao.save(title, body);
 
         return "성공";
-    }
-
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test() {
-
-        return "test";
-    }
-
-    @RequestMapping("/t2")
-    @ResponseBody
-    public Integer t2(Integer num) {
-
-        return num * 2;
-    }
-
-    @RequestMapping("/t3")
-    @ResponseBody
-    public Article t3() {
-        Article article = new Article();
-        article.setId(1L);
-        article.setTitle("title1");
-        article.setBody("body1");
-
-        return article;
-    }
-
-    @RequestMapping("/t4")
-    @ResponseBody
-    public List<Article> t4() {
-        Article article = new Article();
-        article.setId(1L);
-        article.setTitle("title1");
-        article.setBody("body1");
-
-        Article article2 = new Article();
-        article2.setId(2L);
-        article2.setTitle("title2");
-        article2.setBody("body2");
-
-        List<Article> articleList = new ArrayList<>() {{
-            add(article);
-            add(article2);
-        }};
-
-        return articleList;
     }
 }
