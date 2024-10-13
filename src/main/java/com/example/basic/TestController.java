d10df2af8d6d228408a2e78f03e8dbf64a27d88b
suvapor283 <suvapor8632@gmail.com>
2024-10-13 22:55:18 +0900

    thymeleaf parameter additional

--- src/main/java/com/example/basic/TestController.java
+++ src/main/java/com/example/basic/TestController.java
@@ -1,11 +1,18 @@
 package com.example.basic;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class TestController {
+
+    private final ArticleDao articleDao;
 
     @GetMapping("/test/var")
     public String var() {
@@ -23,11 +30,23 @@ public class TestController {
     }
 
     @GetMapping("/test/param")
-    public String param(Model model){
+    public String param(Model model) {
 
         int myNumber = 10;
+        String myString = "hello";
+
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("orange");
+
+        Article article = articleDao.findById(1L);
 
         model.addAttribute("myNumber", myNumber);
+        model.addAttribute("myString", myString);
+        model.addAttribute("fruits", fruits);
+        model.addAttribute("article", article);
 
         return "test/param";
     }
