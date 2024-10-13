e2ee5ced3daab6e1d19c302ca649c0ebab446813
suvapor283 <suvapor8632@gmail.com>
2024-10-13 22:46:14 +0900

    thymeleaf parameter

--- src/main/java/com/example/basic/TestController.java
+++ src/main/java/com/example/basic/TestController.java
@@ -1,6 +1,7 @@
 package com.example.basic;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -19,5 +20,15 @@ public class TestController {
     @GetMapping("/test/loop")
     public String loop() {
         return "test/loop";
+    }
+
+    @GetMapping("/test/param")
+    public String param(Model model){
+
+        int myNumber = 10;
+
+        model.addAttribute("myNumber", myNumber);
+
+        return "test/param";
     }
 }
