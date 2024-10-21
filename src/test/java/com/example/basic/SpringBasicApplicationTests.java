444eb742b437fe843b26cb234543aa8eaa70a01c
suvapor283 <suvapor8632@gmail.com>
2024-10-21 23:39:25 +0900

    member data db save

--- src/test/java/com/example/basic/SpringBasicApplicationTests.java
+++ src/test/java/com/example/basic/SpringBasicApplicationTests.java
@@ -1,31 +1,25 @@
 package com.example.basic;
 
+import com.example.basic.domain.auth.dao.MemberDao;
+import com.example.basic.domain.auth.entity.Member;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 class SpringBasicApplicationTests {
 
+    @Autowired
+    private MemberDao memberDao;
+
     @Test
     void t1() {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
+        Member member = Member.builder()
+                .username("kim")
+                .password("1234")
+                .role("normal")
+                .build();
 
-        List<String> strList = new ArrayList<>();
-        strList.add("aaa");
-        strList.add("bbb");
-        strList.add("ccc");
-
-        for (int i = 0; i < strList.size(); i++) {
-            System.out.println(strList.get(i));
-        }
-
-        for (String str : strList) {
-            System.out.println(str);
-        }
+        memberDao.save(member);
     }
 }
