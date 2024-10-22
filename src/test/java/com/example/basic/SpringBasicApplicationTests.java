90d63d03ea13bb8eba85a0c3a66e2e1e8583b504
suvapor283 <suvapor8632@gmail.com>
2024-10-23 04:18:46 +0900

    article table add member foreign key

--- src/test/java/com/example/basic/SpringBasicApplicationTests.java
+++ src/test/java/com/example/basic/SpringBasicApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.basic;
 
+import com.example.basic.domain.article.entity.Article;
+import com.example.basic.domain.article.repository.ArticleRepository;
 import com.example.basic.domain.auth.entity.Member;
 import com.example.basic.domain.auth.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,9 @@ class SpringBasicApplicationTests {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @Test
     @DisplayName("회원 저장 - save")
@@ -88,5 +93,32 @@ class SpringBasicApplicationTests {
 
             memberRepository.save(member);
         }
+    }
+
+    @Test
+    @DisplayName("Article에 외래키로 memberId 넣어서 저장")
+    void t6() {
+        Member member = new Member();
+        member.setUsername("kim");
+        member.setPassword("1234");
+        member.setRole("admin");
+
+        Article article = new Article();
+        article.setTitle("테스트 제목1");
+        article.setBody("테스트 내용1");
+        article.setMemberId(1L);
+
+        memberRepository.save(member);
+        articleRepository.save(article);
+    }
+
+    @Test
+    @DisplayName("게시물 정보와 작성자 정보 같이 가져오기")
+    void t7() {
+        Article article = articleRepository.findById(1L).get();
+
+        long memberId = article.getMemberId();
+
+        System.out.println(memberRepository.findById(memberId));
     }
 }
