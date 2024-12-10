4b57bda84d74254ba5317c3168f9c75d3f0dc56c
suvapor283 <suvapor8632@gmail.com>
2024-12-10 20:39:28 +0900

    lazy error test

--- src/test/java/com/example/basic/TransactionTest.java
+++ src/test/java/com/example/basic/TransactionTest.java
@@ -1,5 +1,9 @@
 package com.example.basic;
 
+import com.example.basic.domain.article.entity.Article;
+import com.example.basic.domain.article.repository.ArticleRepository;
+import com.example.basic.domain.member.entity.Member;
+import com.example.basic.domain.member.repository.MemberRepository;
 import com.example.basic.test.Account;
 import com.example.basic.test.AccountRepository;
 import jakarta.transaction.Transactional;
@@ -8,6 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,6 +23,12 @@ public class TransactionTest {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @Test
     void t1() {
         System.out.println("test1");
@@ -23,7 +36,7 @@ public class TransactionTest {
 
     @Test
     @DisplayName("계좌 테스트 데이터 생성")
-    void t2(){
+    void t2() {
         Account a1 = new Account();
         a1.setOwner("홍길동");
         a1.setAmount(20000);
@@ -38,7 +51,7 @@ public class TransactionTest {
 
     @Test
     @DisplayName("@Transactional 설정하지 않는 경우")
-    void t3(){
+    void t3() {
         Account a1 = accountRepository.findById(1L).get();
         Account a2 = accountRepository.findById(2L).get();
 
@@ -46,7 +59,7 @@ public class TransactionTest {
         a1.setAmount(a1.getAmount() - 1000);
         accountRepository.save(a1);
 
-        if(true){
+        if (true) {
             throw new RuntimeException("강제로 예외 발생");
         }
 
@@ -57,18 +70,68 @@ public class TransactionTest {
     @Test
     @DisplayName("@Transactional 설정한 경우")
     @Transactional
-    void t4(){
+    void t4() {
         Account a1 = accountRepository.findById(1L).get();
         Account a2 = accountRepository.findById(2L).get();
 
         a1.setAmount(a1.getAmount() - 1000);
         accountRepository.save(a1);
 
-        if(true){
+        if (true) {
             throw new RuntimeException("강제로 예외 발생");
         }
 
         a2.setAmount(a2.getAmount() + 1000);
         accountRepository.save(a2);
+    }
+
+    private Article makeTestArticle(String title, String body, Member author) {
+        return Article.builder()
+                .title(title)
+                .body(body)
+                .author(author)
+                .build();
+    }
+
+    private Member makeTestMember(String username, String password, String role) {
+        return Member.builder()
+                .username(username)
+                .password(password)
+                .role(role)
+                .build();
+    }
+
+    @Test
+    @DisplayName("회원 & 게시물 테스트 데이터 생성")
+    void t5() {
+        System.out.println("====== 테스트 데이터 생성 시작 ======");
+
+        Member m1 = makeTestMember("hong", "1234", "admin");
+        Member m2 = makeTestMember("kim", "qwer", "normal");
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Article a1 = makeTestArticle("제목1", "내용1", m1);
+        Article a2 = makeTestArticle("제목2", "내용2", m1);
+        Article a3 = makeTestArticle("제목3", "내용3", m2);
+
+        articleRepository.save(a1);
+        articleRepository.save(a2);
+        articleRepository.save(a3);
+
+        System.out.println(" ====== 테스트 데이터 등록 완료 ======");
+    }
+
+    @Test
+    @DisplayName("Lazy 로딩")
+    void t6() {
+        Member m1 = memberRepository.findById(1L).get();
+
+        List<Article> articles = m1.getArticles();
+
+        for (Article article : articles) {
+            System.out.println(article.getTitle());
+        }
     }
 }
