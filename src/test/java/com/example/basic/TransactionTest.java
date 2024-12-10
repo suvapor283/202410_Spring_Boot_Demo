548ec1c155617767dd2d6db22c5ea6fd7245f6c2
suvapor283 <suvapor8632@gmail.com>
2024-12-10 20:25:53 +0900

    transaction test

--- src/test/java/com/example/basic/TransactionTest.java
+++ src/test/java/com/example/basic/TransactionTest.java
@@ -2,6 +2,7 @@ package com.example.basic;
 
 import com.example.basic.test.Account;
 import com.example.basic.test.AccountRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,12 +39,36 @@ public class TransactionTest {
     @Test
     @DisplayName("@Transactional 설정하지 않는 경우")
     void t3(){
+        Account a1 = accountRepository.findById(1L).get();
+        Account a2 = accountRepository.findById(2L).get();
 
+        // 2개의 DB 작업은 모두 성공 or 모두 실패 => 트랜잭션
+        a1.setAmount(a1.getAmount() - 1000);
+        accountRepository.save(a1);
+
+        if(true){
+            throw new RuntimeException("강제로 예외 발생");
+        }
+
+        a2.setAmount(a2.getAmount() + 1000);
+        accountRepository.save(a2);
     }
 
     @Test
     @DisplayName("@Transactional 설정한 경우")
+    @Transactional
     void t4(){
+        Account a1 = accountRepository.findById(1L).get();
+        Account a2 = accountRepository.findById(2L).get();
 
+        a1.setAmount(a1.getAmount() - 1000);
+        accountRepository.save(a1);
+
+        if(true){
+            throw new RuntimeException("강제로 예외 발생");
+        }
+
+        a2.setAmount(a2.getAmount() + 1000);
+        accountRepository.save(a2);
     }
 }
